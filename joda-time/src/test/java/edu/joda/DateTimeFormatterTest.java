812a85ec7be1ec9553f941994b60c6f2ee344c35
mishamm <maximchuk.mihail@gmail.com>
2018-08-15 11:56:55 +0300

    add joda.time tests - DateTimeFormatter

---
+++ joda-time/src/test/java/edu/joda/DateTimeFormatterTest.java
@@ -0,0 +1,44 @@
+package edu.joda;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Test;
+
+import java.util.Locale;
+
+import static edu.joda.util.JodaUtils.dateTime;
+import static org.junit.Assert.assertEquals;
+
+public class DateTimeFormatterTest {
+
+    @Test
+    public void print_Pattern_dd_MMMM() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd MMMM");
+        DateTime dateTime = dateTime("2018-08-14 10:15:00");
+
+        String actualFormattedDate = formatter.print(dateTime);
+
+        assertEquals("14 August", actualFormattedDate);
+    }
+
+    @Test
+    public void print_Pattern_dd_MMMM_WithRussianLocale() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd MMMM").withLocale(Locale.forLanguageTag("ru-RU"));
+        DateTime dateTime = dateTime("2018-08-14 10:15:00");
+
+        String actualFormattedDate = formatter.print(dateTime);
+
+        assertEquals("14 августа", actualFormattedDate);
+    }
+
+    @Test
+    public void print_Pattern_dd_MM() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd.MM");
+        DateTime dateTime = dateTime("2018-08-14 10:15:00");
+
+        String actualFormattedDate = formatter.print(dateTime);
+
+        assertEquals("14.08", actualFormattedDate);
+    }
+}
