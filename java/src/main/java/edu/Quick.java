b7c9f6992843106b6d95705c7c8b1e5284fd9b70
mishamm <maximchuk.mihail@gmail.com>
2018-08-19 07:57:33 +0300

    experiments with java - class Objects and null references comparing

--- java/src/main/java/edu/Quick.java
+++ java/src/main/java/edu/Quick.java
@@ -14,5 +14,17 @@ public class Quick {
 
         System.out.println(Double.toString(2.0 - 1.1));
         System.out.println(Float.toString(2.0f - 1.1f));
+
+        System.out.println();
+
+        System.out.println(equals(null, null));
+        Object a = null;
+        Object b = null;
+        System.out.println(equals(a, b));
+    }
+
+    // todo write tests for class Objects
+    private static boolean equals(Object a, Object b) {
+        return (a == b) || (a != null && a.equals(b));
     }
 }
