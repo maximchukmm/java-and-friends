2863ffd864b2b84d702976f2f33a3baa495f652b
mishamm <maximchuk.mihail@gmail.com>
2018-08-19 08:06:05 +0300

    + book controller tests

---
+++ spring/spring-boot-first-look/src/test/java/edu/web/controller/BookControllerTest.java
@@ -0,0 +1,26 @@
+package edu.web.controller;
+
+import edu.persistence.repo.BookRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private BookRepository bookRepository;
+
+    @Test
+    public void findAll_WhenFindAllBooks_ThenOk() {
+
+    }
+}
