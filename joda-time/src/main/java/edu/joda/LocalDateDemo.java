39722f3797b60b7c741f21f677a656fbe8c66884
mmm <maximchuk.mihail@gmail.com>
2018-08-23 20:05:03 +0300

    experiments with joda.time - LocalDate

--- joda-time/src/main/java/edu/joda/LocalDateDemo.java
+++ joda-time/src/main/java/edu/joda/LocalDateDemo.java
@@ -8,35 +8,38 @@ import org.joda.time.YearMonth;
 
 class LocalDateDemo {
     public static void main(String[] args) {
-        LocalDate localDate = new LocalDate(2018, 8, 10);
+//        LocalDate localDate = new LocalDate(2018, 8, 10);
+//
+//        System.out.println(localDate.toDateTimeAtStartOfDay());
+//        System.out.println(localDate.toDateTimeAtMidnight());
+//        System.out.println(localDate.toDateTimeAtStartOfDay(DateTimeZone.UTC));
+//        System.out.println(localDate.toDateTimeAtMidnight(DateTimeZone.UTC));
+//        System.out.println(localDate.toDateTimeAtStartOfDay(DateTimeZone.forID("Europe/Moscow")));
+//        System.out.println(localDate.toDateTimeAtMidnight(DateTimeZone.forID("Europe/Moscow")));
+//
+//        System.out.println();
+//
+//        YearMonth yearMonth = new YearMonth(localDate);
+//        System.out.println(yearMonth);
+//
+//        System.out.println();
+//
+//        Weeks week = Weeks.weeks(localDate.getWeekyear());
+//        System.out.println(week);
+//
+//        System.out.println();
+//
+//        System.out.println(localDate.getWeekOfWeekyear());
+//        System.out.println(localDate.getWeekyear());
+//        System.out.println(new Week(localDate.getWeekOfWeekyear(), localDate.getWeekyear()));
+//
+//        System.out.println();
+//
+//        for (int i = 0; i < 7; i++) {
+//            System.out.println(localDate.plusDays(i) + " - day of week - " + localDate.plusDays(i).getDayOfWeek());
+//        }
 
-        System.out.println(localDate.toDateTimeAtStartOfDay());
-        System.out.println(localDate.toDateTimeAtMidnight());
-        System.out.println(localDate.toDateTimeAtStartOfDay(DateTimeZone.UTC));
-        System.out.println(localDate.toDateTimeAtMidnight(DateTimeZone.UTC));
-        System.out.println(localDate.toDateTimeAtStartOfDay(DateTimeZone.forID("Europe/Moscow")));
-        System.out.println(localDate.toDateTimeAtMidnight(DateTimeZone.forID("Europe/Moscow")));
-
-        System.out.println();
-
-        YearMonth yearMonth = new YearMonth(localDate);
-        System.out.println(yearMonth);
-
-        System.out.println();
-
-        Weeks week = Weeks.weeks(localDate.getWeekyear());
-        System.out.println(week);
-
-        System.out.println();
-
-        System.out.println(localDate.getWeekOfWeekyear());
-        System.out.println(localDate.getWeekyear());
-        System.out.println(new Week(localDate.getWeekOfWeekyear(), localDate.getWeekyear()));
-
-        System.out.println();
-
-        for (int i = 0; i < 7; i++) {
-            System.out.println(localDate.plusDays(i) + " - day of week - " + localDate.plusDays(i).getDayOfWeek());
-        }
+        for (int i = 0; i < 50; i++)
+            System.out.println(LocalDate.now().plusDays(i).getDayOfWeek());
     }
 }
