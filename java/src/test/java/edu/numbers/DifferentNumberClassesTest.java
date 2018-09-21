705ecfc4d2820d40ad29704f89faaab582a36428
mmm <maximchuk.mihail@gmail.com>
2018-09-21 19:55:41 +0300

    add java tests - integer division

--- java/src/test/java/edu/numbers/DifferentNumberClassesTest.java
+++ java/src/test/java/edu/numbers/DifferentNumberClassesTest.java
@@ -13,4 +13,25 @@ public class DifferentNumberClassesTest {
 
         assertEquals(9L, longValue);
     }
+
+    @Test
+    public void integer_WhenDividendLessDivisor_ThenReturnZero() {
+        assertEquals(0, 0 / 60);
+        assertEquals(0, 1 / 60);
+        assertEquals(0, 29 / 60);
+        assertEquals(0, 30 / 60);
+        assertEquals(0, 31 / 60);
+        assertEquals(0, 59 / 60);
+    }
+
+    @Test
+    public void round_WhenDividendLessThanHalfOfDivisor_ThenReturnZero() {
+        assertEquals(0, Math.round(29 / 60.0));
+    }
+
+    @Test
+    public void round_WhenDividendLessThanDivisorButEqualOrGreaterThanHalfOfDivisor_ThenReturnOne() {
+        assertEquals(1, Math.round(30 / 60.0));
+        assertEquals(1, Math.round(31 / 60.0));
+    }
 }
