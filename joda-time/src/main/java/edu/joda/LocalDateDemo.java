f64c9816aa3fada61dd6d20845f4edef46b066cd
mmm <maximchuk.mihail@gmail.com>
2018-09-21 19:55:04 +0300

    experiments with joda.time - LocalDate

--- joda-time/src/main/java/edu/joda/LocalDateDemo.java
+++ joda-time/src/main/java/edu/joda/LocalDateDemo.java
@@ -1,10 +1,12 @@
 package edu.joda;
 
 import org.jfree.data.time.Week;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.joda.time.Weeks;
-import org.joda.time.YearMonth;
+import org.joda.time.*;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 class LocalDateDemo {
     public static void main(String[] args) {
@@ -39,7 +41,16 @@ class LocalDateDemo {
 //            System.out.println(localDate.plusDays(i) + " - day of week - " + localDate.plusDays(i).getDayOfWeek());
 //        }
 
-        for (int i = 0; i < 50; i++)
-            System.out.println(LocalDate.now().plusDays(i).getDayOfWeek());
+//        for (int i = 0; i < 50; i++)
+//            System.out.println(LocalDate.now().plusDays(i).getDayOfWeek());
+
+        System.out.println(getListDaysByPeriod(new LocalDate(2018, 9, 1), new LocalDate(2018, 9, 9  )));
+    }
+
+    public static List<LocalDate> getListDaysByPeriod(org.joda.time.LocalDate from, org.joda.time.LocalDate to) {
+        int numOfDays = Days.daysBetween(from, to).getDays() + 1;
+        return Stream.iterate(from, date -> date.plusDays(1))
+            .limit(numOfDays)
+            .collect(toList());
     }
 }
