d3ebb7b3881c704f77e1d947414d7f748751153e
mmm <maximchuk.mihail@gmail.com>
2018-10-05 16:13:16 +0300

    joda.time tests - MinutesBefore local time

--- joda-time/src/test/java/edu/joda/MinutesTest.java
+++ joda-time/src/test/java/edu/joda/MinutesTest.java
@@ -1,11 +1,14 @@
 package edu.joda;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
 import org.joda.time.Minutes;
 import org.junit.Test;
 
 import static edu.joda.util.JodaUtils.dateTime;
+import static edu.joda.util.JodaUtils.localTime;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MinutesTest {
     @Test
@@ -37,6 +40,54 @@ public class MinutesTest {
 
         int expectedMinutesBetween = -1;
         int actualMinutesBetween = Minutes.minutesBetween(from, to).getMinutes();
+
+        assertEquals(expectedMinutesBetween, actualMinutesBetween);
+    }
+
+    @Test
+    public void minutesBetween_WhenFirstLocalTimeBeforeSecondLocalTime_ThenReturnPositiveInteger() {
+        LocalTime start = localTime("10:00:00");
+        LocalTime end = localTime("11:00:00");
+
+        int expectedMinutesBetween = 60;
+        int actualMinutesBetween = Minutes.minutesBetween(start, end).getMinutes();
+
+        assertTrue(actualMinutesBetween > 0);
+        assertEquals(expectedMinutesBetween, actualMinutesBetween);
+    }
+
+    @Test
+    public void minutesBetween_WhenFirstLocalTimeAfterSecondLocalTime_ThenReturnNegativeInteger() {
+        LocalTime start = localTime("11:00:00");
+        LocalTime end = localTime("10:00:00");
+
+        int expectedMinutesBetween = -60;
+        int actualMinutesBetween = Minutes.minutesBetween(start, end).getMinutes();
+
+        assertTrue(actualMinutesBetween < 0);
+        assertEquals(expectedMinutesBetween, actualMinutesBetween);
+    }
+
+    //todo name
+    @Test
+    public void test5() {
+        LocalTime start = localTime("23:00:00");
+        LocalTime end = localTime("01:00:00");
+
+        int expectedMinutesBetween = 120;
+        int actualMinutesBetween = Minutes.minutesBetween(start, end).getMinutes();
+
+        assertEquals(expectedMinutesBetween, actualMinutesBetween);
+    }
+
+    //todo name
+    @Test
+    public void test6() {
+        LocalTime start = localTime("01:00:00");
+        LocalTime end = localTime("23:00:00");
+
+        int expectedMinutesBetween = 120;
+        int actualMinutesBetween = Minutes.minutesBetween(start, end).getMinutes();
 
         assertEquals(expectedMinutesBetween, actualMinutesBetween);
     }
