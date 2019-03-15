c1b786b479f4d1e23005b735786a89c080f91aa5
maximchukmm <maximchuk.mihail@gmail.com>
2019-03-15 17:48:49 +0300

    add java tests - Arrays

---
+++ java/src/test/java/edu/arrays/ArraysTest.java
@@ -0,0 +1,17 @@
+package edu.arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ArraysTest {
+
+    @Test
+    public void copyOfRange_whenCopyRangeOfArray_ThenRightBorderIsExclusive() {
+        Assert.assertArrayEquals(
+            new int[]{4, 5, 6},
+            Arrays.copyOfRange(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8}, 4, 7)
+        );
+    }
+}
